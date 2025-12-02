0f41c5326e4ebc80045bea9dad7ae4a4335b8d42
jgim54077 <jgim54077@gmail.com>
2025-12-02 15:59:08 +0900

    Add files via upload
    현재 가마 태우기 게임은 잘 돌아가고, 내가 쓴 형식에 맞춰서 이어서 추가해주면 좋을듯
    다시 깃허브에 올릴때 어떤거 추가했는지 지금처럼 description에 써주셈

--- project/project.java
+++ project/project.java
@@ -11,11 +11,15 @@ public class project {
     }
 }
 
-//opening
+
+// 오프닝 및 튜토리얼
+//======================================================================================================================
+
+// 오프닝
 class Frame1 extends JFrame {
     Image background;
 
-    //Frame1(오프닝 장면)
+    // 화면 사이즈 및 제목 설정
     public Frame1() {
         setTitle("King's Escape : 아관파천");
         setSize(1500, 800);
@@ -52,7 +56,7 @@ class Frame1 extends JFrame {
         tutorialBtn.setBounds(100, 650, 200, 40);
         panel.add(tutorialBtn);
 
-        // 튜토리얼 -> Frame2로 이동
+        // 튜토리얼 -> Frame3로 이동
         tutorialBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -74,7 +78,8 @@ class Frame1 extends JFrame {
         setVisible(true);
     }
 }
-//tutorial
+
+// 튜토리얼
 class Frame2 extends JFrame {
     Image background;
 
@@ -91,9 +96,10 @@ class Frame2 extends JFrame {
             }
         };
 
+        // frame3로 넘어가는 버튼
         panel.setLayout(null);
         JButton button2 = new JButton("고종 태우기 시작!!");
-        button2.setBounds(350, 400, 200, 40);
+        button2.setBounds(600, 400, 200, 40);
         panel.add(button2);
 
         button2.addActionListener(new ActionListener() {
@@ -108,15 +114,15 @@ class Frame2 extends JFrame {
     }
 }
 
-//게임1: 고종이랑 무게 맞추기
+// 게임1: 고종과 엄순헌 무게 맞추기
 //======================================================================================================================
+
+// 고종 태우기 게임 장면1(뒷배경)
 class Frame3 extends JFrame {
     Image background;
 
-    //고종 태우기 게임 시작
     public Frame3() {
         background = new ImageIcon("src/project/frame3_bg.png").getImage();
-        JFrame frame3 = new JFrame("게임 시작! 고종 빼돌리기");
         setTitle("고종 가마 태우기 시작");
         setSize(1500, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -135,15 +141,13 @@ class Frame3 extends JFrame {
         button3.setContentAreaFilled(false);
         button3.setBorderPainted(false);
         button3.setFocusPainted(false);
-
         button3.setForeground(Color.WHITE);
-
-        button3.setBounds(350, 400, 1500, 800);
+        button3.setBounds(0, 0, 1500, 800);
         panel.add(button3);
 
         button3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                new Frame3();
+                new Frame4();
                 dispose();
             }
         });
@@ -153,14 +157,17 @@ class Frame3 extends JFrame {
     }
 }
 
+// 고종 태우기 게임 장면2(가마 등장)
 class Frame4 extends JFrame {
     Image background;
 
     public Frame4() {
-        background = new ImageIcon("src/project/frame4_bg.png").getImage();
-        setTitle("고종 가마 태우기 게임 중");
+        background = new ImageIcon("src/project/frame3_bg.png").getImage();
+        setTitle("고종 가마 태우기 시작");
         setSize(1500, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(0, 0);
+        setVisible(true);
 
         JPanel panel = new JPanel() {
             public void paintComponent(Graphics g) {
@@ -168,13 +175,16 @@ class Frame4 extends JFrame {
                 g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
             }
         };
-
         panel.setLayout(null);
-        JButton button4 = new JButton("다음");
-        button4.setBounds(100, 100, 200, 40);
-        panel.add(button4);
+        JButton button3 = new JButton("");
+        button3.setContentAreaFilled(false);
+        button3.setBorderPainted(false);
+        button3.setFocusPainted(false);
+        button3.setForeground(Color.WHITE);
+        button3.setBounds(350, 400, 1500, 800);
+        panel.add(button3);
+        button3.addActionListener(new ActionListener() {
 
-        button4.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 new Frame5();
                 dispose();
@@ -186,192 +196,291 @@ class Frame4 extends JFrame {
     }
 }
 
+// 고종 태우기 게임 장면3(본게임)
+class Frame5 extends JFrame {
+    Image background;
+    Image gojongImg;
+    Image umImg;
+
+    // 캐릭터 시작 위치
+    int gojongX = 450;
+    int gojongY = -100;
+    int umX = 850;
+    int umY = -100;
+
+    // 캐릭터 크기
+    int charWidth = 100;
+    int charHeight = 150;
+
+    // 떨어지는 속도
+    int gravity = 3;
+
+    // 가마 좌표
+    int gamaTopY = 510; // 가마 윗라인
+
+    // 아직 캐릭터가 공중에 있는 상태
+    boolean gojongLand = false;
+    boolean umLand = false;
+
+    // 게임 중 타이머
+    Timer timer;
+
+    public Frame5() {
+        background = new ImageIcon("src/project/frame5_bg.png").getImage();
+        gojongImg = new ImageIcon("src/project/gojong.png").getImage();
+        umImg = new ImageIcon("src/project/umsun.png").getImage();
+
+        setTitle("고종 가마 균형 맞추기");
+        setSize(1500, 800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(0, 0);
+
+        GamePanel panel = new GamePanel();
+        setContentPane(panel);
+        setVisible(true);
+    }
+
+    // 게임 화면 전제(패널 내부 클래스)
+    class GamePanel extends JPanel implements KeyListener {
+        public GamePanel() {
+            setFocusable(true);
+            requestFocusInWindow();
+            addKeyListener(this);
+
+            // 떨어지는 타이머(0.02초마다 반복 실행-> 매 프레임마다 캐릭터 위치 업데이트)
+            timer = new Timer(20, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (!gojongLand) { // 고종이 멈추면
+                        gojongY += gravity;
+                        if (gojongY + charHeight >= gamaTopY) {
+                            gojongY = gamaTopY - charHeight;
+                            gojongLand = true;
+                        }
+                    }
+                    if (!umLand) { // 엄순헌이 멈추면
+                        umY += gravity;
+                        if (umY + charHeight >= gamaTopY) {
+                            umY = gamaTopY  - charHeight;
+                            umLand = true;
+                        }
+                    }
+                    repaint();
+                }
+            });
+
+            timer.start();
+
+            // 판정 버튼
+            setLayout(null);
+            JButton btn = new JButton("가자~!");
+            btn.setBounds(700, 700, 120, 40);
+            add(btn);
+
+            btn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    check();
+                }
+            });
+        }
+
+        // 키보드 입력
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int code = e.getKeyCode();
+
+            // 고종 이동
+            if (code == KeyEvent.VK_A) gojongX -= 10;
+            if (code == KeyEvent.VK_D) gojongX += 10;
+
+            // 엄순헌 이동
+            if (code == KeyEvent.VK_J) umX -= 10;
+            if (code == KeyEvent.VK_L) umX += 10;
+
+            repaint();
+        }
+        @Override
+        public void keyReleased(KeyEvent e) {}
+        @Override
+        public void keyTyped(KeyEvent e) {}
+
+        // 성공 or 실패 판정
+        void check() {
+            int gamaStart = 380; // 가마 좌측선
+            int gamaEnd = 1100;
+            boolean gojongOK = (gojongX >= gamaStart - 100 && gojongX + charWidth <= gamaEnd - 100);
+            boolean umOK = (umX >= gamaStart - 200 && umX + charWidth <= gamaEnd - 200);
+
+            if (gojongLand && umLand && gojongOK && umOK) new Frame6();
+            else new Frame15();
+
+            timer.stop();
+            SwingUtilities.getWindowAncestor(this).dispose();
+        }
+
+        // 배경 및 캐릭터 그리기
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(background, 0, 0, getWidth(), getHeight(), null); // 배경
+            g.drawImage(gojongImg, gojongX, gojongY, charWidth, charHeight, null); // 고종
+            g.drawImage(umImg, umX, umY, charWidth, charHeight, null); // 엄순헌
+        }
+    }
+
+}
 
 
+class Frame6 extends JFrame {
+    Image background;
 
+    public Frame6() {
+        background = new ImageIcon("src/project/frame6_bg.png").getImage();
+    }
+}
+
+
+//게임2 : 타이밍에 맞춰 엄순헌 얼굴 내밀기
+//======================================================================================================================
 
 
 //Ending=========================================================================================================================================Ending
-class Frame12 extends JFrame {
-    Image background;
+// 해피엔딩 장면1(러시아인)
+    class Frame12 extends JFrame {
+        Image background;
 
-    //고종 태우기 게임 시작
-    public Frame12() {
-        background = new ImageIcon("src/project/frame12_bg.png").getImage();
-        JFrame frame12 = new JFrame("해피엔딩0: 무사통과");
-        setTitle("일본 순사 통과");
-        setSize(1500, 800);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocation(0, 0);
-        setVisible(true);
+        public Frame12() {
+            background = new ImageIcon("src/project/frame12_bg.png").getImage();
+            setTitle("일본 순사 통과");
+            setSize(1500, 800);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setLocation(0, 0);
+            setVisible(true);
 
-        JPanel panel = new JPanel() {
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+            JPanel panel = new JPanel() {
+                public void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
 
-            }
-        };
-        panel.setLayout(null);
+                }
+            };
+            panel.setLayout(null);
 
-        //다음 장면 이동 버튼 만들기
-        JButton button12 = new JButton("");
-        button12.setContentAreaFilled(false);
-        button12.setBorderPainted(false);
-        button12.setFocusPainted(false);
-        button12.setForeground(Color.WHITE);
-        button12.setBounds(350, 400, 1500, 800);
-        panel.add(button12);
-        //패널에 붙이기
+            //다음 장면 이동 버튼 만들기
+            JButton button12 = new JButton("");
+            button12.setContentAreaFilled(false);
+            button12.setBorderPainted(false);
+            button12.setFocusPainted(false);
+            button12.setForeground(Color.WHITE);
+            button12.setBounds(350, 400, 1500, 800);
+            panel.add(button12); //패널에 붙이기
 
-        //해피엔딩 1로 이동
-        button12.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Frame13();
-                dispose();
-            }
-        });
+            // 해피엔딩 장면2(글)로 이동
+            button12.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    new Frame13();
+                    dispose();
+                }
+            });
 
-        add(panel);
-        setVisible(true);
+            add(panel);
+            setVisible(true);
+        }
     }
-}
 
+    // 해피엔딩 장면2(글)
+    class Frame13 extends JFrame {
+        Image background;
 
+        public Frame13() {
+            background = new ImageIcon("src/project/frame13_bg.png").getImage();
+            setTitle("해피엔딩 안내글");
+            setSize(1500, 800);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setLocation(0, 0);
+            setVisible(true);
 
-class Frame13 extends JFrame {
-    Image background;
+            JPanel panel = new JPanel() {
+                public void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
 
-    //
-    public Frame13() {
-        background = new ImageIcon("src/project/frame13_bg.png").getImage();
-        JFrame frame13 = new JFrame("해피엔딩1: 러시아 공사관 도착");
-        setTitle("러시아 공사관 도착");
-        setSize(1500, 800);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocation(0, 0);
-        setVisible(true);
+                }
+            };
+            panel.setLayout(null);
 
-        JPanel panel = new JPanel() {
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+            //다음 장면 이동 버튼 만들기
+            JButton button13 = new JButton("");
+            button13.setContentAreaFilled(false);
+            button13.setBorderPainted(false);
+            button13.setFocusPainted(false);
 
-            }
-        };
-        panel.setLayout(null);
+            button13.setForeground(Color.WHITE);
 
-        //다음 장면 이동 버튼 만들기
-        JButton button13 = new JButton("");
-        button13.setContentAreaFilled(false);
-        button13.setBorderPainted(false);
-        button13.setFocusPainted(false);
+            button13.setBounds(350, 400, 1500, 800);
+            panel.add(button13); //패널에 붙이기
 
-        button13.setForeground(Color.WHITE);
-
-        button13.setBounds(350, 400, 1500, 800);
-        panel.add(button13);
-        //패널에 붙이기
-
-        //해피엔딩 2로 이동
-        button13.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Frame14();
-                dispose();
-            }
-        });
-        add(panel);
-        setVisible(true);
+            //해피엔딩 장면3으로 이동
+            button13.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    new Frame14();
+                    dispose();
+                }
+            });
+            add(panel);
+            setVisible(true);
+        }
     }
-}
 
-class Frame14 extends JFrame {
-    Image background;
+    // 해피엔딩 장면3(학생들)
+    class Frame14 extends JFrame {
+        Image background;
 
-    //고종 태우기 게임 시작
-    public Frame14() {
-        background = new ImageIcon("src/project/frame14_bg.png").getImage();
-        JFrame frame14 = new JFrame("해피엔딩2: 소개문구");
-        setTitle("해피엔딩 소개문구");
-        setSize(1500, 800);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocation(0, 0);
-        setVisible(true);
+        public Frame14() {
+            background = new ImageIcon("src/project/frame14_bg.png").getImage();
+            setTitle("고종을 위한 감사표시 :)");
+            setSize(1500, 800);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setLocation(0, 0);
+            setVisible(true);
 
-        JPanel panel = new JPanel() {
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+            JPanel panel = new JPanel() {
+                public void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
 
-            }
-        };
-        panel.setLayout(null);
-
-        //다음 장면 이동 버튼 만들기
-        JButton button14 = new JButton("");
-        button14.setContentAreaFilled(false);
-        button14.setBorderPainted(false);
-        button14.setFocusPainted(false);
-        button14.setForeground(Color.WHITE);
-        button14.setBounds(350, 400, 1500, 800);
-        panel.add(button14);//패널에 붙이기
-
-        //해피엔딩 2로 이동
-        button14.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Frame15();
-                dispose();
-            }
-        });
-        add(panel);
-        setVisible(true);
+                }
+            };
+            panel.setLayout(null);
+            add(panel);
+            setVisible(true);
+        }
     }
-}
 
-class Frame15 extends JFrame {
-    Image background;
+    // 배드엔딩
+    class Frame15 extends JFrame {
+        Image background;
 
-    //고종 태우기 게임 시작
-    public Frame15() {
-        background = new ImageIcon("src/project/frame15_bg.png").getImage();
-        JFrame frame15 = new JFrame("해피엔딩1: 러시아 공사관 도착");
-        setTitle("러시아 공사관 도착");
-        setSize(1500, 800);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocation(0, 0);
-        setVisible(true);
+        public Frame15() {
+            background = new ImageIcon("src/project/frame15_bg.png").getImage();
+            setTitle("러시아 공사관 도착 실패..");
+            setSize(1500, 800);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setLocation(0, 0);
 
-        JPanel panel = new JPanel() {
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+            JPanel panel = new JPanel() {
+                public void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
 
-            }
-        };
-        panel.setLayout(null);
+                }
+            };
 
-        //다음 장면 이동 버튼 만들기
-        JButton button15 = new JButton("");
-        button15.setContentAreaFilled(false);
-        button15.setBorderPainted(false);
-        button15.setFocusPainted(false);
-        button15.setForeground(Color.WHITE);
-        button15.setBounds(350, 400, 1500, 800);
-        panel.add(button15);
-        //패널에 붙이기
-
-        //해피엔딩 2로 이동
-        button15.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Frame15();
-                dispose();
-            }
-        });
-        add(panel);
-        setVisible(true);
+            panel.setLayout(null);
+            add(panel);
+            setVisible(true);
+        }
     }
-}
